a1c11f9464b375dabf19815c992242ecbbcd102c
haighr <rowan.haigh@dfo-mpo.gc.ca>
2024-04-26 10:32:18 -0700

    PBSstewdio 0.0.1 (2024-04-25) [Rcheck=F, Rbuild=F]   * Creation     + From the mind of JTS, a new package is born       - RH likes the connotation of poking a stick into Hadley Wickham's eye (Rstudio)

---
+++ PBSstewdio/R/PBSstew.r
@@ -0,0 +1,70 @@
+## stew---------------------------------2024-04-25
+## Starts the primary GUI interface
+## Authors: Jon T. Schnute, Rowan Haigh
+## ---------------------------------------------RH
+stew <- function(pkg="PBSstewdio", wdf="stewWin.txt") #, pathfile="ADpaths.txt")
+{
+#	.initOptions()
+#	if (!is.null(pathfile) && file.exists(pathfile))
+#		readADpaths(pathfile)
+#	else pathfile="ADpaths.txt"
+#	pkg="PBSpkg"
+
+	## Perhaps rename to something else -- too similar to .PBSadmb
+#	assign("PBSadmb",list(pkg=pkg,call=match.call(),args=args(admb),useCols=NULL),envir=.PBSadmbEnv)
+
+	pdir <- system.file(package=pkg)          ## package directory
+	wdir <- paste(pdir,"/win",sep="")         ## window description file directory
+	#pdir <- "C:/Users/haighr/Files/Projects/R/Source/pbs-software/pbs-stewdio/trunk/PBSstewdio"  ## just fornow
+	#wdir <- "C:/Users/haighr/Files/Projects/R/Source/pbs-software/pbs-stewdio/trunk/PBSstewdio/inst/win"  ## just fornow
+#	edir <- paste(pdir,"/examples",sep="")    ## examples directory
+	tdir <- tempdir()                         ## temporary working directory
+	twdf <- paste(tdir,"/",wdf,sep="")        ## temporary window description file
+	twdf <- convSlashes(twdf,os="unix")
+
+	stripExt <- function(x) { return(sub("[.].{1,3}$", "", x)) }
+
+	win.filename <- paste(wdir,wdf,sep="/")
+	temp <- readLines(win.filename)
+	
+	## insert examples into window description file menuitems
+#	etpl <- basename(Sys.glob(file.path(edir,"*.tpl"))) ## TPL files in examples directory
+#	eprf <- stripExt(etpl)                              ## strip off extensions
+#	enew <- character(0)
+#	edir <- gsub( "\\\\", "/", edir )
+#	for (i in eprf) 
+#		enew=c(enew,paste("menuitem label=",i," function=doAction action=\"copyFiles(`",
+#			i,".`,srcdir=`",edir,"`); convOS(paste(`",i,"`,c(`.tpl`,`.dat`,`.pin`,`.r`),sep=``))\"",sep=""))
+#	temp <- gsub("@nitems",length(eprf),temp)
+#	temp <- gsub("@menuitems",paste(enew,collapse="\n\t"),temp)
+#
+#	temp = gsub("@wdf",twdf,temp)
+#	temp = gsub("@pathfile",pathfile,temp)
+	
+	## create the window (from temp string)
+	temp <- unlist( strsplit(temp, "\n" ) )
+	## createWin(twdf, TRUE)
+	writeLines(temp,con=twdf)
+	createWin(twdf)
+
+	## set some values
+#	.load.prefix.droplist()
+#	loadOptionsGUI( atcall(.PBSadmb) )
+#	isOK <- .win.checkADopts()
+#
+#	if( isOK == FALSE && .Platform$OS.type == "windows" ) {
+#		cat( "\nADMB or MinGW are not installed in the default location.\n" )
+#		cat( "If needed, follow install instructions from GUI menu <Install>\n" )
+#		cat( "If ADMB is installed on your system, you can manually set the ADMB or MinGW path values\n" )
+#		cat( "in the GUI to point to your own installations.\n" )
+#	}
+
+	#TODO need centralized window variable init (is it done anywhere?)
+#	setWinVal( list( currentdir.values = getwd() ) )
+#	setWinVal( list( currentdir = getwd() ) )
+#	#setWinVal( list( optfile = optfile ) )
+#	setWinVal( list( optfile = pathfile ) )
+	invisible()
+}
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~pkg
+#stew()
---
+++ PBSstewdio/R/zzz.r
@@ -0,0 +1,24 @@
+.onAttach <- function(lib,pkg)
+{
+	pkg_info = utils::sessionInfo( package="PBSstewdio" )$otherPkgs$PBSstewdio
+	if( is.character( pkg_info$Packaged ) )
+		pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
+	else
+		pkg_date  <- date()
+	
+	#userguide_path <- system.file( "doc/PBSawatea.pdf", package = "PBSawatea" )
+	year <- substring(date(),nchar(date())-3,nchar(date()))
+
+	packageStartupMessage("
+-----------------------------------------------------------
+PBS Stewdio ", pkg_info$Version, " -- Copyright (C) 2024-",year," Fisheries and Oceans Canada
+
+Packaged on ", pkg_date, "
+Pacific Biological Station, Nanaimo
+
+All available PBS packages can be found at
+https://github.com/pbs-software
+-----------------------------------------------------------
+
+")
+}
